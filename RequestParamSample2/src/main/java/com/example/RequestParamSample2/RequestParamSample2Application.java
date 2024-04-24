25eeb8b433803e36f0dfe022c6b17b726a46e638
honghuiseung <huiseung1082@daum.net>
2024-04-24 10:35:16 +0900

    PathVariableSample

---
+++ RequestParamSample2/src/main/java/com/example/RequestParamSample2/RequestParamSample2Application.java
@@ -0,0 +1,13 @@
+package com.example.RequestParamSample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestParamSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestParamSample2Application.class, args);
+	}
+
+}
